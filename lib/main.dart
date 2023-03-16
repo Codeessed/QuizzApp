b917a0c0f83ca796a3584986becfcd3457c425c0
Gbohunmi Makinde <gbohunmimakinde@gmail.com>
2023-03-16 18:22:20 +0100

    home screen done

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Scaffold(body: MyHomePage(title: 'Quizz Page')),
     );
   }
 }
@@ -32,84 +32,108 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  int btnIndex = 0;
+  // final List<bool> _selections = List.generate(2, (_)=>false );
+  late bool buttonEnabled;
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+  void initState() {
+    super.initState();
+    buttonEnabled = false;
+  }
+
+  Widget customRadioButton(String text, int index, IconData icon) {
+    buttonEnabled = btnIndex > 0;
+
+    return OutlinedButton(
+      onPressed: () {
+        setState(() {
+          btnIndex = index;
+        });
+      },
+
+      style: OutlinedButton.styleFrom(
+        side: BorderSide(width: 2, color: (btnIndex == index) ? Colors.green : Colors.black)
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+      // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      // borderSide: BorderSide(color: (value == index) ? Colors.green : Colors.black),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          children: [
+             Icon(icon,size: 40, color: (btnIndex == index) ? Colors.green : Colors.black),
+            const SizedBox(width: 20),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              text,
+              style: TextStyle(
+                color: (btnIndex == index) ? Colors.green : Colors.black,
+              ),
+            ),
+            const Spacer(),
+            Text(
+              '0%',
+              style: TextStyle(
+                color: (btnIndex == index) ? Colors.green : Colors.black,
+                // colo: btnIndex == index? Colors.green : Colors.black;
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return  Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            customRadioButton("Flutter", 1,  Icons.flutter_dash),
+            const SizedBox(height: 20),
+            customRadioButton("Html", 2, Icons.html),
+            const SizedBox(height: 50),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: buttonEnabled? Colors.green : Colors.grey
+                ),
+                onPressed: (){
+                  setState(() {
+                    btnIndex = 0;
+                  });
+                  buttonEnabled? showSnackBar(context, 'Move to next page'): showSnackBar(context, 'Pick a quizz');
+                  // setState(() {
+                  //   buttonEnabled = !buttonEnabled;
+                  // });
+                },
+                child: const Text("Continue")
+            )
+          ],
+        ),
+      ),
+    );
+
+  }
+
+  void showSnackBar(BuildContext context, String text){
+    ScaffoldMessenger.of(context)..removeCurrentSnackBar()..showSnackBar(SnackBar(content: Text(text)));
+  }
+
 }
+
